01a110112e54bd2c00a96864de52b2066f13244a
priyanka2889 <priyanka.bluesys@gmail.com>
2015-10-07 11:30:09 +0530

    push

--- js/js_index.js
+++ js/js_index.js
@@ -47,45 +47,45 @@
 			{
 				localStorage.userId= data.userId;
 				localStorage.username= data.username;
-				$("#ok").attr("href", 'js_dashboard.html');
+				/* $("#ok").attr("href", 'js_dashboard.html');
 				$("#pop_msg_reset").popup('open');
 				$('#pop_msg_reset p').text('Login Successfully ');
-				
-				/*navigator.notification.confirm("Login"+data.username+" Successfully",registrationCallBack, "Confirmation", "Ok");
+				*/
+				navigator.notification.confirm("Login"+data.username+" Successfully",registrationCallBack, "Confirmation", "Ok");
 					$.mobile.loading('show');
 					function registrationCallBack(button){
 				$.mobile.loading('hide');
 					if(button == 1) {
 						window.location.href = 'js_dashboard.html';
 					}
-				}*/
+				}
 				}
 			else if(data.status== '0'){
-				alert("Login Fail:Invalid Username or Password");
-				/*navigator.notification.alert(
+				//alert("Login Fail:Invalid Username or Password");
+				navigator.notification.alert(
 						'Invalid Username or Password',  // message
 						 null,         // callback
 						'Login Fail:',            // title
 						'Ok'                  // buttonName
-				);*/
+				);
 				} 
 			else if(data.status== '2'){
-				alert("You are not registered");
-				/*navigator.notification.alert(
+				//alert("You are not registered");
+				navigator.notification.alert(
 					'You are not registered',  // message
 					null,         // callback
 					'Login Fail:',            // title
 					'Ok'                  // buttonName
-					);*/
+					);
 			}
 			else if(data.status== '3'){
-				alertpopup("Please Check your UserType");
-				/*navigator.notification.alert(
+				//alertpopup("Please Check your UserType");
+				navigator.notification.alert(
 					'Please Check your UserType',  // message
 					null,         // callback
 					'Login Fail:',            // title
 					'Ok'                  // buttonName
-					);*/
+					);
 				
 				}
 	
@@ -94,13 +94,13 @@
          {  
 		 
 		 
-			alert("Connection Error");
-			/*navigator.notification.alert(
+			//alert("Connection Error");
+	     	navigator.notification.alert(
 			'Error',  // message
 			null,         // callback
-			'Something went wrong:',            // title
-			'Ok'                  // buttonName
-			);*/
+			'Connection Error',            // title
+			'OK'                  // buttonName
+			);
          }  
 		 /* function OnError(xhr, errorType, exception) 
 		 {
